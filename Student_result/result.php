689724fe4e67b5b0af9d1130bf4e908604654d73
yasin arafat <ashekhyasin@gmail.com>
2024-10-19 00:31:32 +0600

    first commit

---
+++ Student_result/result.php
@@ -0,0 +1,37 @@
+<?php
+
+$marks=55;
+
+switch($marks){
+    case $marks<=100 && $marks>=80;
+    echo "Your Grade is A+";
+    break;
+
+    case $marks<80 && $marks>=70;
+    echo "Your Grade is A";
+    break;
+
+    case $marks<70 && $marks>=60;
+    echo "Your Grade is B";
+    break;
+
+    case $marks<60 && $marks>=50;
+    echo "Your Grade is C";
+    break;
+
+    case $marks<50 && $marks>=40;
+    echo "Your Grade is D";
+    break;
+
+    case $marks<40 && $marks>=33;
+    echo "Your Grade is E";
+    break;
+
+    case $marks<33 && $marks>=0;
+    echo "Your Grade is F (you have marks less than 33)";
+    break;
+
+    default:
+    echo "Enter Valid Marks";
+    break;
+}
