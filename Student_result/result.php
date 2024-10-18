14c99bfce13d2a385eafd81635330985a7264ac0
yasin arafat <ashekhyasin@gmail.com>
2024-10-19 00:54:34 +0600

    new line

--- Student_result/result.php
+++ Student_result/result.php
@@ -12,19 +12,19 @@ switch($marks){
     break;
 
     case $marks<70 && $marks>=60;
-    echo "Your Grade is B";
+    echo "Your Grade is A-";
     break;
 
     case $marks<60 && $marks>=50;
-    echo "Your Grade is C";
+    echo "Your Grade is B";
     break;
 
     case $marks<50 && $marks>=40;
-    echo "Your Grade is D";
+    echo "Your Grade is C";
     break;
 
     case $marks<40 && $marks>=33;
-    echo "Your Grade is E";
+    echo "Your Grade is D";
     break;
 
     case $marks<33 && $marks>=0;
